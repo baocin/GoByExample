0210a011d2d8b7d077bd6f286490149a6461d582
baocin <steele.pedersen@gmail.com>
2014-12-31 21:47:38 -0500

    The switch-case example

---
+++ switch.go
@@ -0,0 +1,37 @@
+// switch
+//This was the bad commit
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("Congrats!~ It's the weekend!")
+	default:
+		fmt.Println("Another boring weekday")
+	}
+
+	t := time.Now()
+	//alternative if statement
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon!")
+	default:
+		fmt.Println("It's after noon...")
+	}
+}
