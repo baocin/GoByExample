e2d70a72322f3b77a9e4ed7a6f2e26233509b059
baocin <steele.pedersen@gmail.com>
2014-12-31 21:43:00 -0500

    The if-else example

--- if-else.go
+++ if-else.go
@@ -6,5 +6,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	if 7%2 == 0 { //if 7 is even
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative") //should never see this
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+	fmt.Println(`"Done"`)
+
 }
